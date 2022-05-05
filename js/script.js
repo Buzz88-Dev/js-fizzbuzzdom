eb3cff83b00f791c8c5927759ead8dd0bb8d0479
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-05 16:46:55 +0200

    script ended

--- js/script.js
+++ js/script.js
@@ -31,3 +31,25 @@ for (let i = 1; i <= 100; i++) {
     }
 
 }
+
+
+// Extra
+// creo una variabile let e la collego al div con classe foot
+let divconh3 = document.querySelector("div.foot");
+
+// creo una variabile let; con essa creo un elemento h3 dentro al div.foot
+let creoh3 = document.createElement("h3");
+
+// all elemento h3 aggiungo una classe "classFooter" e un altra classe "altraclasse" utilizzando uno di questi due modi
+creoh3.setAttribute("class","classFooter");
+creoh3.classList.add("altraclasse");
+
+// scrivo il testo di h3
+creoh3.append("Footer");
+
+// inserisco il testo di h3 dentro al div.foot cosi che si possa visualizzare sul browser
+divconh3.append(creoh3);
+
+
+// con classList.remove rimuovo le classi
+creoh3.classList.remove("altraclasse");
